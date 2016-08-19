c19c0db67304ab284843246ff3478cb3045e6966
skybaby <dream.boy.2015@icloud.com>
2016-08-19 15:32:15 +0800

    Revert "测试类修改"
    This reverts commit 1eac5fecccd09a66ed48b43deffd5aae1d037cea.

--- src/test/decorator/DecoratorTest.java
+++ src/test/decorator/DecoratorTest.java
@@ -10,10 +10,6 @@ public class DecoratorTest {
 		// 礼品盒 包装 包装袋的月饼
 		BoxDecorator boxDecorator = new BoxDecorator(bagDecorator);
 		boxDecorator.show();
-		//针对接口编程
-		component = new BagDecorator(component);
-		component = new BoxDecorator(component);
-		component.show();
 
 	}
 
